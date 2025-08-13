298d5909ab94e9913cf70ae2793cda578e07f562
Igor F. Duca <yelldutz@gmail.com>
2025-08-13 15:11:22 -0300

    feat: adding cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({ origin: 'http://localhost:3000' });
   await app.listen(process.env.PORT ?? 3000);
 }
 
